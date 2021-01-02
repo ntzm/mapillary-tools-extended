769cf6f6857505f9fb5785c917a7284420cefa77
Nat Zimmermann <nat@ntzm.io>
2021-01-02 15:54:41 +0000

    use pathbuf

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@ use indicatif::{ProgressBar, ProgressStyle};
 use rayon::prelude::*;
 use rexiv2;
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
-    let paths: Vec<String> = fs::read_dir("data")
+    let paths: Vec<PathBuf> = fs::read_dir("data")
         .unwrap()
-        .map(|f| f.unwrap().path().to_str().unwrap().to_string())
+        .map(|f| f.unwrap().path())
         .collect();
 
     rexiv2::set_log_level(rexiv2::LogLevel::ERROR);
@@ -24,7 +25,7 @@ fn main() {
             let meta = rexiv2::Metadata::new_from_path(&path)?;
             let date = meta.get_tag_string("Exif.GPSInfo.GPSDateStamp")?;
             let time = meta.get_tag_interpreted_string("Exif.GPSInfo.GPSTimeStamp")?;
-            // Source for capacity: YY:MM:DD HH:II:SS
+            // Source for capacity: YY:MM:DD HH:MM:SS
             let mut date_time = String::with_capacity(19);
             date_time.push_str(&date);
             date_time.push_str(" ");
