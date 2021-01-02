5147bdc10729479e8fed08310adc8e3f503edfd2
Nat Zimmermann <nat@ntzm.io>
2021-01-02 15:44:14 +0000

    wip

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use indicatif::ParallelProgressIterator;
+use indicatif::{ProgressBar, ProgressStyle};
+use rayon::prelude::*;
+use rexiv2;
+use std::fs;
+
+fn main() {
+    let paths: Vec<String> = fs::read_dir("data")
+        .unwrap()
+        .map(|f| f.unwrap().path().to_str().unwrap().to_string())
+        .collect();
+
+    rexiv2::set_log_level(rexiv2::LogLevel::ERROR);
+
+    let progress = ProgressBar::new(paths.len() as u64);
+    progress.set_style(ProgressStyle::default_bar().template(
+        "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} (ETA: {eta})",
+    ));
+
+    let errors: Vec<rexiv2::Rexiv2Error> = paths
+        .par_iter()
+        .progress_with(progress)
+        .map(|path| {
+            let meta = rexiv2::Metadata::new_from_path(&path)?;
+            let date = meta.get_tag_string("Exif.GPSInfo.GPSDateStamp")?;
+            let time = meta.get_tag_interpreted_string("Exif.GPSInfo.GPSTimeStamp")?;
+            // Source for capacity: YY:MM:DD HH:II:SS
+            let mut date_time = String::with_capacity(19);
+            date_time.push_str(&date);
+            date_time.push_str(" ");
+            date_time.push_str(&time);
+            meta.set_tag_string("Exif.Photo.DateTimeOriginal", &date_time)?;
+            meta.save_to_file(&path)
+        })
+        .filter_map(|r| r.err())
+        .collect();
+
+    for error in errors {
+        println!("{}", error.to_string());
+    }
+}
